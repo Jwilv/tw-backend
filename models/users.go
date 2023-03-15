8142adc17e869bf73e4ec3743b01ca516e05f5a8
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-03-15 20:27:41 -0300

    model User

---
+++ models/users.go
@@ -0,0 +1,23 @@
+package models
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+
+)
+
+//User es el modelo de Usuario para la base de datos en mongoDb
+type User struct {
+	ID        primitive.ObjectID `bson:"_id,omitempty" json:"id"`
+	Name      string             `bson:"name" json:"name"`
+	Surname   string             `bson:"surname" json:"surname"`
+	BirthDate time.Time          `bson:"birthDate" json:"birthDate"`
+	Email     string             `bson:"email" json:"email"`
+	Password  string             `bson:"password" json:"password,omitempty"`
+	Avatar    string             `bson:"avatar" json:"avatar,omitempty"`
+	Banner    string             `bson:"banner" json:"banner,omitempty"`
+	Biography string             `bson:"biography" json:"biography,omitempty"`
+	Location  string             `bson:"location" json:"location,omitempty"`
+	Website   string             `bson:"website" json:"website,omitempty"`
+}
