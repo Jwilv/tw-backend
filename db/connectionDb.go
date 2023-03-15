31ae0984a7b9df3d62aa57df84ad9f302c16e107
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-03-15 16:08:02 -0300

    MongoCN

--- db/connectionDb.go
+++ db/connectionDb.go
@@ -1,7 +1,15 @@
 package db
 
 import (
+	"context"
+
+	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+
 )
 
+var MongoCN = connectionDb()
+
 var clientOptions = options.Client().ApplyURI("")
+
+
