6f7a480de63e987135253cb84b1ff096178d6664
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-03-15 17:06:20 -0300

    comprobacion de connect

--- db/connectionDb.go
+++ db/connectionDb.go
@@ -2,10 +2,10 @@ package db
 
 import (
 	"context"
+	"log"
 
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
-
 )
 
 var MongoCN = connectionDb()
@@ -14,4 +14,9 @@ var clientOptions = options.Client().ApplyURI("")
 
 func connectionDb() *mongo.Client {
 	client, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		log.Fatal(err)
+		return client
+	}
+
 }
