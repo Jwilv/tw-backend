3e9d605d7aaef3c03310a2efebe17484238bd756
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-03-17 12:23:42 -0300

    new decoder body

--- routers/register.go
+++ routers/register.go
@@ -1,6 +1,7 @@
 package routers
 
 import (
+	"encoding/json"
 	"net/http"
 
 	"github.com/Jwilv/tw-backend/models"
@@ -9,4 +10,5 @@ import (
 // Register es la funcion que nos permite registrar un nuevo usuario en la base de datos
 func Register(w http.ResponseWriter, r *http.Request) {
 	var user models.User
+	err := json.NewDecoder(r.Body)
 }
