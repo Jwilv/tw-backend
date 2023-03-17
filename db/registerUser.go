9fc34aec15cab13640c7e5b327efe3b7a49f0161
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-03-17 20:03:56 -0300

    RegisterUser

---
+++ db/registerUser.go
@@ -0,0 +1,8 @@
+package db
+
+import "github.com/Jwilv/tw-backend/models"
+
+//RegisterUser es la funcion que obtiene como parametro el user y lo registra en la base de datos
+func RegisterUser(user models.User) (string,bool,error){
+
+}
