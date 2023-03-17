caba093f92468a13811ac787bb583e4c1caeab5d
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-03-17 13:08:36 -0300

    error 400 en los datos

--- routers/register.go
+++ routers/register.go
@@ -5,10 +5,15 @@ import (
 	"net/http"
 
 	"github.com/Jwilv/tw-backend/models"
+
 )
 
 // Register es la funcion que nos permite registrar un nuevo usuario en la base de datos
 func Register(w http.ResponseWriter, r *http.Request) {
 	var user models.User
-	err := json.NewDecoder(r.Body)
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		http.Error(w, "Error en los datos recibidos " + err.Error(), 400)
+	}
+
 }
