82c138b6594c5c2dc3b39859a7869060cfc48f44
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-03-18 20:54:47 -0300

    collection

---
+++ db/checkExisEmail.go
@@ -0,0 +1,22 @@
+package db
+
+import (
+	"context"
+	"time"
+
+	"github.com/Jwilv/tw-backend/models"
+	"go.mongodb.org/mongo-driver/bson"
+
+)
+
+// CheckExisEmail verificamos si ya existe un usuario con el email que de parametro
+func CheckExisEmail(email string) (models.User, bool, string) {
+	context, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	defer cancel()
+
+	db := MongoCN.Database("tw")
+	collection := db.Collection("users")
+
+
+	
+}
