961241af3d033d65d43e31aeed7588ba60a84f83
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-03-18 21:53:34 -0300

    IntentLogin

---
+++ db/intentLogin.go
@@ -0,0 +1,26 @@
+package db
+
+import (
+	"github.com/Jwilv/tw-backend/models"
+	"golang.org/x/crypto/bcrypt"
+
+)
+
+//IntentLogin es la funcion que noos permite logearnos 
+func IntentLogin(email string, password string) (models.User, bool) {
+	user, exist, _ := CheckExisEmail(email)
+	if !exist {
+		return user, false
+	}
+
+	passwordBytes := []byte(password)
+	passwordBytesDB := []byte(user.Password)
+
+	err := bcrypt.CompareHashAndPassword(passwordBytesDB, passwordBytes)
+
+	if err != nil {
+		return user, false
+	}
+
+	return user, true
+}
