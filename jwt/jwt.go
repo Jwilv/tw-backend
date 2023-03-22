b2f589bdefa1c296b89d2e37e1a3f1215e4b1125
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-03-22 14:43:31 -0300

    payload

--- jwt/jwt.go
+++ jwt/jwt.go
@@ -1,10 +1,24 @@
 package jwt
 
 import (
+	"time"
+
 	"github.com/Jwilv/tw-backend/models"
+	"github.com/dgrijalva/jwt-go"
 )
 
 func GenerateJwt(user models.User) (string, error) {
 	key := []byte("oqwepFJO03-49RFSAFN0123@$WEWQAsfoajf")
+	payload := jwt.MapClaims{
+		"email":     user.Email,
+		"name":      user.Name,
+		"surname":   user.Surname,
+		"birthDate": user.BirthDate,
+		"biography": user.Biography,
+		"location":  user.Location,
+		"website":   user.Website,
+		"_id":       user.ID.Hex(),
+		"exp":       time.Now().Add(time.Hour * 24).Unix(),
+	}
 
 }
