23adf5912f4d885be3b606d53cb0de9654ad1ba7
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-03-24 17:15:46 -0300

    coments

--- db/getProfile.go
+++ db/getProfile.go
@@ -17,23 +17,32 @@ func GetProfile(ID string) (models.User, error) {
 	db := MongoCN.Database("tw")
 	collection := db.Collection("users")
 
+	//variable para el perfil, le asignamos el modelo user
 	var profile models.User
 
+	//pasamos el string del id a objetId
 	objectId, _ := primitive.ObjectIDFromHex(ID)
 
+	//es la concion a cumplir, que la _id tenga el mismo valor que la id que llego por parametro
 	condition := bson.M{
 		"_id": objectId,
 	}
 
+	//buscamosun registro en la collection, le damos elcontext, la condition
+	//despues decode en el espacio de memoria de profile
+	// y si sale mal nos da err
 	err := collection.FindOne(context, condition).Decode(&profile)
 
+	//le asignamos unas password vacia aunque sea omitida
 	profile.Password = ""
 
+	//evaluamos sinos dio err y devolvemos el profile y el err
 	if err != nil {
 		fmt.Println("registro no  encontrado" + err.Error())
 		return profile, err
 	}
 
+	//retornamos el profile y err en nil
 	return profile, nil
 
 }
