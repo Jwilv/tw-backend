7d87503a4711060e81a34f0727daa78cc16c81b4
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-03-28 16:46:07 -0300

    ReadNotes

---
+++ db/readNote.go
@@ -0,0 +1,54 @@
+package db
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"github.com/Jwilv/tw-backend/models"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo/options"
+
+)
+
+func ReadNotes(ID string, page int64) ([]*models.ReturnNotes, bool) {
+
+	contextDb, cancel := context.WithTimeout(context.Background(), time.Second*15)
+	defer cancel()
+
+	db := MongoCN.Database("tw")
+	collection := db.Collection("notes")
+
+	var result []*models.ReturnNotes
+
+	condition := bson.M{
+		"userId": ID,
+	}
+
+	options := options.Find()
+	options.SetLimit(20)
+	options.SetSort(bson.D{{Key: "date", Value: -1}})
+
+	options.SetSkip((page - 1) * 20)
+
+	cursor, err := collection.Find(contextDb, condition, options)
+
+	if err != nil {
+		log.Fatal(err.Error())
+		return result, false
+	}
+
+	for cursor.Next(context.TODO()) {
+		var document models.ReturnNotes
+		err := cursor.Decode(&document)
+
+		if err != nil{
+			return result, false 
+		}
+
+		result = append(result, &document )
+	}
+
+	return result, true 
+
+}
