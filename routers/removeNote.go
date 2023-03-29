ce6522907e20199a6e4372740f27f57597e74e75
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-03-29 16:20:16 -0300

    RemoveNote

--- routers/removeNote.go
+++ routers/removeNote.go
@@ -11,10 +11,18 @@ func RemoveNote(w http.ResponseWriter, r *http.Request) {
 	ID := r.URL.Query().Get("id")
 
 	if len(ID) < 1 {
-		http.Error(w, "id invalido", 400)
+		http.Error(w, "id invalido", http.StatusBadRequest)
 		return
 	}
 
-	err := db.DeleteNote(ID)
+	err := db.DeleteNote(ID, IDUser)
+
+	if err != nil {
+		http.Error(w, "error al intentar borrar la nota"+err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	w.Header().Set("Content-type", "application/json")
+	w.WriteHeader(http.StatusCreated)
 
 }
