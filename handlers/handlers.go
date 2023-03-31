fbef46463491cab7a7c16405789280b457b2eca6
wilvers <juan.ignacio.wilvers@gmail.com>
2023-03-31 18:29:46 -0300

    comentarios para explicar las rutas

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -14,18 +14,36 @@ import (
 
 // Drivers manejo de seteo y ListenServe
 func Drivers() {
-	router := mux.NewRouter()
 
+	//Router objet
+	router := mux.NewRouter()
+	//route register, crea user en la base de datos 
 	router.HandleFunc("/register", middlewares.CheckDb(routers.Register)).Methods("POST")
+	//route login, se encarga de logear la data contra la base de datos
 	router.HandleFunc("/login", middlewares.CheckDb(routers.Login)).Methods("POST")
+	//obtiene un id y devuelve un modelo user que se encuentre en la base de datos. que contenga la id otorgada
 	router.HandleFunc("/getprofile", middlewares.CheckDb(middlewares.ValidateJwt(routers.GetProfile))).Methods("GET")
+	//obtiene data y la pasa a un modelo de user, para despues buscarlo en la base de datos y modificarlo 
 	router.HandleFunc("/changeProfile", middlewares.CheckDb(middlewares.ValidateJwt(routers.ChangeProfile))).Methods("PUT")
+	//obtiene la data y la pasa a un modelo de note para despues subirlo a la base de datos 
 	router.HandleFunc("/saveNote", middlewares.CheckDb(middlewares.ValidateJwt(routers.SaveNote))).Methods("POST")
+	//se obtiene las notas de la base de datos y se le pasa a un arreglo, se obtiene todas las notas del user.
+	// ya que se envia el id de dicho user
 	router.HandleFunc("/getNotes", middlewares.CheckDb(middlewares.ValidateJwt(routers.GetNotes))).Methods("GET")
+	//route removeNote, obtiene el id del user por el jwt y se le envia el id de la nota a eliminar.
+	//y la remueve de la base de datos
 	router.HandleFunc("/removeNote", middlewares.CheckDb(middlewares.ValidateJwt(routers.RemoveNote))).Methods("DELETE")
+	//route updateAvatar, se encarga de recibir un archivo, crea un archivo en local y modifica o agrega
+	//el path a la base de datos en el campo avatar
 	router.HandleFunc("/updateAvatar", middlewares.CheckDb(middlewares.ValidateJwt(routers.SaveAvatar))).Methods("POST")
+	//route updateBanner, se encarga de recibir un archivo, crea un archivo en local y modifica o agrega
+	//el path a la base de datos en el campo banner
 	router.HandleFunc("/updateBanner", middlewares.CheckDb(middlewares.ValidateJwt(routers.SaveBanner))).Methods("POST")
+	//obtiene el path de la base de datos del campo banner y busca el archivo local para copiarlo
+	//en la respuesta y asi enviar el archivo
 	router.HandleFunc("/getBanner", middlewares.CheckDb(routers.GetBanner)).Methods("GET")
+	//obtiene el path de la base de datos del campo  avatar y busca el archivo local para copiarlo
+	//en la respuesta y asi enviar el archivo
 	router.HandleFunc("/getAvatar", middlewares.CheckDb(routers.GetAvatar)).Methods("GET")
 
 
