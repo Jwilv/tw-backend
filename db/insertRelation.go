b8a4acb2a654fbc3efe6601015276d92914ea06f
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-04-03 15:01:28 -0300

    limitaciones para banner

---
+++ db/insertRelation.go
@@ -0,0 +1,15 @@
+package db
+
+// import (
+//
+//
+//
+//
+
+// 	"github.com/Jwilv/tw-backend/models"
+
+// )
+
+// func InsertRelation(relation models.Relation) (bool, error) {
+// 	contextDb, cancel := context.wit
+// }
