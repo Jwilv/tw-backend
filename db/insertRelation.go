30125704dc8ca8069d6d87e75f65612230391381
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-04-03 21:28:06 -0300

    InsertRelation

--- db/insertRelation.go
+++ db/insertRelation.go
@@ -1,15 +1,26 @@
 package db
 
-// import (
-//
-//
-//
-//
+import (
+	"context"
+	"time"
 
-// 	"github.com/Jwilv/tw-backend/models"
+	"github.com/Jwilv/tw-backend/models"
 
-// )
+)
 
-// func InsertRelation(relation models.Relation) (bool, error) {
-// 	contextDb, cancel := context.wit
-// }
+//inserta la relacion en la base de datos
+func InsertRelation(relation models.Relation) (bool, error) {
+	contextDb, cancel := context.WithTimeout(context.Background(), time.Second * 15)
+	defer cancel()
+
+	Db := MongoCN.Database("tw")
+	collection := Db.Collection("relation")
+
+	_, err := collection.InsertOne(contextDb, relation)
+
+	if err != nil {
+		return false, err
+	}
+
+	return true, nil
+}
