7e1f186d6049703d6dfaa292cb25b925bcac6e91
wilvers <juan.ignacio.wilvers@gmail.com>
2023-05-16 19:42:47 -0300

    RemoveMsg

---
+++ routers/removemsg.go
@@ -0,0 +1,28 @@
+package routers
+
+import (
+	"net/http"
+
+	"github.com/Jwilv/tw-backend/db"
+)
+
+//RemoveMsg elimina una nota de la base de datos
+func RemoveMsg(w http.ResponseWriter, r *http.Request) {
+	ID := r.URL.Query().Get("id")
+
+	if len(ID) < 1 {
+		http.Error(w, "id invalido", http.StatusBadRequest)
+		return
+	}
+
+	err := db.DeleteMsg(ID, IDUser)
+
+	if err != nil {
+		http.Error(w, "error al intentar borrar la nota"+err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	w.Header().Set("Content-type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+
+}
