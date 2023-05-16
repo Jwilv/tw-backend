18a7165a99175260fc6cca211780d72d8e29c44c
wilvers <juan.ignacio.wilvers@gmail.com>
2023-05-16 18:54:21 -0300

    MsgAdmin

---
+++ db/msgAdmin.go
@@ -0,0 +1,37 @@
+package db
+
+import (
+	"context"
+	"time"
+
+	"github.com/Jwilv/tw-backend/models"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+// MsgAdmin graba la nota en la base de datos
+func MsgAdmin(note models.RegisterNote) (string, bool, error) {
+
+	context, cancel := context.WithTimeout(context.Background(), time.Second*15)
+	defer cancel()
+
+	db := MongoCN.Database("tw")
+	collection := db.Collection("msgAdm")
+
+	register := bson.M{
+		"userId":  note.UserId,
+		"message": note.Message,
+		"date":    note.Date,
+		"name":    note.Name,
+	}
+
+	result, err := collection.InsertOne(context, register)
+
+	if err != nil {
+		return string(""), false, err
+	}
+
+	objId, _ := result.InsertedID.(primitive.ObjectID)
+
+	return objId.String(), true, nil
+}
