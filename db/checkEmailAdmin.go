9c6f0dfdaca5c53901dffd3c8f4378de7037ccc7
wilvers <juan.ignacio.wilvers@gmail.com>
2023-05-16 18:32:19 -0300

    CheckEmailAdmin

---
+++ db/checkEmailAdmin.go
@@ -0,0 +1,34 @@
+package db
+
+import (
+	"context"
+	"time"
+
+	"github.com/Jwilv/tw-backend/models"
+	"go.mongodb.org/mongo-driver/bson"
+
+)
+
+// CheckExisEmail verificamos si ya existe un usuario con el email que de parametro
+func CheckEmailAdmin(email string) (models.User, bool, string) {
+	context, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	defer cancel()
+
+	db := MongoCN.Database("tw")
+	collection := db.Collection("admin")
+
+	condition := bson.M{"email":email}
+
+	var result models.User
+
+	err := collection.FindOne(context, condition).Decode(&result)
+
+	ID := result.ID.Hex()
+
+	if err != nil {
+		return result, false, ID
+	}
+
+	return result, true, ID
+	
+}
