8519a5b63f996e393693aae2d46e38162eedc33d
wilvers <juan.ignacio.wilvers@gmail.com>
2023-05-22 18:39:48 -0300

    RandomUsers

---
+++ db/randomUsers.go
@@ -0,0 +1,37 @@
+package db
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"github.com/Jwilv/tw-backend/models"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+func RandomUsers() []*models.User  {
+	context, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	defer cancel()
+
+	db := MongoCN.Database("tw")
+	collection := db.Collection("users")
+
+	pipeline := mongo.Pipeline{
+		// Obtén una muestra aleatoria de 4 documentos
+		{{Key: "$sample", Value: bson.M{"size": 4}}},
+	}
+
+	cursor, err := collection.Aggregate(context, pipeline)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer cursor.Close(context)
+
+	var notes []models.ReturnNotes
+	if err = cursor.All(context, &notes); err != nil {
+		log.Fatal(err)
+	}
+
+	return notes
+}
