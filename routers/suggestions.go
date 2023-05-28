4211561703c480b26546ca6ac267af2dbe9db288
wilvers <juan.ignacio.wilvers@gmail.com>
2023-05-28 18:12:42 -0300

    suggestions

---
+++ routers/suggestions.go
@@ -0,0 +1,17 @@
+package routers
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/Jwilv/tw-backend/db"
+)
+
+func suggestions(w http.ResponseWriter, r *http.Request){
+
+	notes := db.ReadRandomNotes(30)
+
+	w.Header().Set("Content-type","application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(&notes)
+}
