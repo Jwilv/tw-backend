102956d27f1079c6cd0cf75d4a3b33d78a384876
wilvers <juan.ignacio.wilvers@gmail.com>
2023-06-08 18:36:47 -0300

    2 intent url database

--- db/connectionDb.go
+++ db/connectionDb.go
@@ -12,7 +12,7 @@ import (
 //MongoCN es el objeto se conexion a nuestra base de datos
 var MongoCN = ConnectionDb()
 
-var clientOptions = options.Client().ApplyURI("mongodb+srv://mongo:9wOn3E9enmNYRWpEw1IC@cluster0.sxvlsh2.mongodb.net/tw")
+var clientOptions = options.Client().ApplyURI("mongodb://mongo:9wOn3E9enmNYRWpEw1IC@containers-us-west-176.railway.app:6664/tw")
 
 //ConnectionDb me permite conectarme a la data base
 func ConnectionDb() *mongo.Client {
